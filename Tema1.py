fc46b508a5190746cf9fd84c34a31de33a433b88
George <60367142+gferaru@users.noreply.github.com>
2020-02-01 22:01:15 +0000

    Update Tema1.py

--- Tema1.py
+++ Tema1.py
@@ -4,7 +4,9 @@ i = int(input("Alege_numar: "))
 while i != j:
     if i < j:
         print ("Caut un numar mai mare")
+        i = int(input("Alege_numar: "))
     if i > j:
-    print ("Caut un numar mai mic")
+        print ("Caut un numar mai mic")
+        i = int(input("Alege_numar: "))
 else:
     print ("Corect!")
