2c253aacd8b826c274df73a2f157804601191a22
George <gferaru@gmail.com>
2020-02-09 18:06:19 +0000

    Update Tema4.py

--- Tema4.py
+++ Tema4.py
@@ -1,3 +1,8 @@
+#Scrieti o functie care afiseaza pe ecran numerele de la 1 la 100, fiecare pe un rand.
+#Daca numarul este multiplu de 3, afisati "Fizz" in loc de acel numar.
+#Daca numarul este multiplu de 5, afisati "Buzz" in loc de acel numar.
+#Daca numarul este multiplu si de 3 si de 5, afisati "FizzBuzz" in loc de acel numar.
+
 def f(k):
     if k % 3 == 0 and k % 5 == 0:
         print("FizzBuzz")
