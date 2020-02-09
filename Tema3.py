aa527ccf16d16452840128cb4e19903caf7ac167
George <gferaru@gmail.com>
2020-02-09 18:05:25 +0000

    Update Tema3.py

--- Tema3.py
+++ Tema3.py
@@ -1,3 +1,8 @@
+#Scrieti o functie care se foloseste de for sa parcurga numerele de la 0 la 100.
+#Daca numarul este par, va fi adunat intr-o alta variabila, si se va trece la urmatorul numar.
+#Daca numarul este mai mic decat 10, va fi afisat pe ecran.
+#Daca numarul este intre 10 si 20, vom trece la urmatorul numar.
+
 def f(k):
     if k % 2 == 0:
         var = int (x * (x + 2) / 4)
